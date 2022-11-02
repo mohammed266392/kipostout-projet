c81f0a2bf630e99d69a1ca927cbe8f8d6b1ebc80
raki <mohammed.raki26@gmail.com>
2022-11-02 23:54:39 +0100

    version template

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,4 +12,23 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  nosRealisation(event : Event){
+    console.log("j'ai cliqué sur nos réalisation",event);
+    const divRealisation = event.composedPath();
+    console.log("event ",divRealisation[1]);
+    const divContent : HTMLDivElement = document.querySelector('.header_menu_items_realisation_content') as HTMLDivElement;
+    if(divContent.style.display == "flex"){
+      divContent.style.display = "none";
+    }else{
+      divContent.style.display = "flex";
+    }
+    
+  }
+
+  afficheMenu(){
+    console.log("j'affiche le menu");
+    const btn  : HTMLDivElement = document.querySelector('.header_menu-burger') as HTMLDivElement ;
+    btn.classList.toggle('active');
+  }
+
 }
