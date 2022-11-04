9861fb03586d046e191644f1abdc7da0060dcb7a
raki <mohammed.raki26@gmail.com>
2022-11-04 22:07:33 +0100

    mobile bouton

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  modaleMobileMenu : boolean = false;
+  showContainer: boolean = false;
+  constructor(public breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    // this.breakpointObserver
+    //   .observe(['(min-width: 982px)'])
+    //   .subscribe((state: BreakpointState) => {
+    //     const btn1  : HTMLDivElement = document.querySelector('.header_menu') as HTMLDivElement ;
+    //     if (state.matches) {
+    //       this.showContainer = true;
+          
+    //   //btn1.classList.toggle('width30');
+    //       btn1.style.width = "auto";
+      
+    //     } else {
+    //       this.showContainer = false;
+    //       btn1.style.width = "0%";
+    //     }
+    //   });
   }
 
   nosRealisation(event : Event){
@@ -28,7 +46,24 @@ export class HeaderComponent implements OnInit {
   afficheMenu(){
     console.log("j'affiche le menu");
     const btn  : HTMLDivElement = document.querySelector('.header_menu-burger') as HTMLDivElement ;
+    const btn1  : HTMLDivElement = document.querySelector('.header_menu') as HTMLDivElement ;
     btn.classList.toggle('active');
+    btn1.classList.toggle('active');
+    // const btn1  : HTMLDivElement = document.querySelector('.header_menu') as HTMLDivElement ;
+    // if(!this.modaleMobileMenu){
+    //   //btn1.classList.toggle('width30');
+    //   btn1.style.width = "30%";
+      
+    // }else{
+    //   //btn1.classList.toggle('width0');
+    //   btn1.style.width = "0%";
+    // }
+    // this.modaleMobileMenu = !this.modaleMobileMenu;
+  }
+
+  afficheMenuMobile(){
+  
+    console.log("j'affiche le menu en mobile");
   }
 
 }
