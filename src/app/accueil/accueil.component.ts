3c0677831031f5548d89ab6cd10fd334b8c642e1
raki <mohammed.raki26@gmail.com>
2022-11-11 19:50:13 +0100

    version 1 3 components

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,27 +1,12 @@
-import { AfterContentInit, AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.scss']
 })
-export class AccueilComponent implements OnInit, AfterContentInit  {
+export class AccueilComponent  {
 
   constructor() { }
-
-  ngAfterContentInit(): void {
-    
-    // divContent.style.backgroundColor="#ffffff94";
-    // divContent.style.width = "313px";
-  }
-
-  ngAfterViewInit(): void {
-    
-  }
-
-  ngOnInit(): void {
-    // const divContent : HTMLDivElement = document.querySelector('.main_title_texte') as HTMLDivElement;
-    // divContent.classList.toggle("active");
-  }
 
 }
