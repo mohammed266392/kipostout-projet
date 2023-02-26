f76ac6576b624fee61c82ed59f510c639b3f679f
raki <mohammed.raki26@gmail.com>
2023-02-26 19:36:20 +0100

    1.0 change balise title v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
-import { DecrireTonProjetComponent } from './decrire-ton-projet/decrire-ton-projet.component';
-import { EtreRappeleComponent } from './etre-rappele/etre-rappele.component';
 import { MaintenanceComponent } from './maintenance/maintenance.component';
 
 const routes: Routes = [
